50af3819ba5d606c309f821b02c6dddd9a7b947e
xukangping <xukp20@mails.tsinghua.edu.cn>
2023-03-26 10:44:29 +0800

    plane and triangle

--- PA1/code/include/plane.hpp
+++ PA1/code/include/plane.hpp
@@ -12,22 +12,44 @@
 class Plane : public Object3D {
 public:
     Plane() {
-
+        _n = Vector3f::UP;
+        _d = 0;
     }
 
     Plane(const Vector3f &normal, float d, Material *m) : Object3D(m) {
-
+        _n = normal;
+        _d = d;
     }
 
     ~Plane() override = default;
 
     bool intersect(const Ray &r, Hit &h, float tmin) override {
+        // 2023/3/26
+        // ray origin to plane
+        
+        // if parallel, no intersection
+        float n_dot_rd = Vector3f::dot(_n, r.getDirection());
+        if (n_dot_rd == 0) {
+            return false;
+        }
+
+        float t = - (_d + Vector3f::dot(_n, r.getOrigin())) / n_dot_rd;
+        if (t < tmin || t <= 0) {
+            return false;
+        }
+
+        if (t <= h.getT()) {
+            Vector3f normal = Vector3f::dot(_n, r.getDirection()) < 0 ? _n : -_n;
+            h.set(t, material, normal);
+            return true;
+        }
         return false;
     }
 
 protected:
-
-
+    // 2023/3/26 add n, d
+    Vector3f _n;
+    float _d;
 };
 
 #endif //PLANE_H
--- PA1/code/include/triangle.hpp
+++ PA1/code/include/triangle.hpp
@@ -15,12 +15,41 @@ public:
 
     // a b c are three vertex positions of the triangle
 	Triangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, Material* m) : Object3D(m) {
-
+		vertices[0] = a;
+		vertices[1] = b;
+		vertices[2] = c;
+		normal = Vector3f::cross(b - a, c - a);
+		normal.normalize();
 	}
 
 	bool intersect( const Ray& ray,  Hit& hit , float tmin) override {
-        return false;
+        // 2023/3/26
+		Vector3f e1 = vertices[0] - vertices[1];
+		Vector3f e2 = vertices[0] - vertices[2];
+		Vector3f s = vertices[0] - ray.getOrigin();
+
+		Vector3f result = Vector3f::ZERO;
+		float under = det(ray.getDirection(), e1, e2);
+		result[0] = det(s, e1, e2) / under;
+		result[1] = det(ray.getDirection(), s, e2) / under;
+		result[2] = det(ray.getDirection(), e1, s) / under;
+
+		if (result[0] < tmin || result[0] > hit.getT() || result[1] < 0 || result[2] < 0 || result[1] + result[2] > 1) {
+			return false;
+		} else {
+			// the direction is opposite to the ray direction?
+			Vector3f n = Vector3f::dot(normal, ray.getDirection()) < 0 ? normal : -normal;
+			hit.set(result[0], material, n);
+			return true;
+		}
 	}
+
+	// new tool function, to compute the det of three vectors
+	float det(const Vector3f& a, const Vector3f& b, const Vector3f& c) {
+		// det equals to the dot product of a and the cross product of b and c -> mixed product
+		return Vector3f::dot(a, Vector3f::cross(b, c));
+	}
+
 	Vector3f normal;
 	Vector3f vertices[3];
 protected:
