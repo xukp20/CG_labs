4f0e5c8a8a790018ae18b4d2fb7341c79ce7140f
xukangping <xukp20@mails.tsinghua.edu.cn>
2023-03-26 11:14:37 +0800

    Group

--- PA1/code/include/group.hpp
+++ PA1/code/include/group.hpp
@@ -15,11 +15,13 @@ class Group : public Object3D {
 public:
 
     Group() {
-
+        group_size = 0;
+        objects = std::vector<Object3D*>();
     }
 
     explicit Group (int num_objects) {
-
+        objects = std::vector<Object3D*>(num_objects);
+        group_size = num_objects;
     }
 
     ~Group() override {
@@ -27,19 +29,26 @@ public:
     }
 
     bool intersect(const Ray &r, Hit &h, float tmin) override {
-
+        bool isIntersect = false;
+        for (int i = 0; i < group_size; i++) {
+            if (objects[i]->intersect(r, h, tmin)) {
+                isIntersect = true;
+            }
+        }
+        return isIntersect;
     }
 
     void addObject(int index, Object3D *obj) {
-
+        objects[index] = obj;
     }
 
     int getGroupSize() {
-
+        return group_size;
     }
 
 private:
-
+    int group_size;
+    std::vector<Object3D*> objects;
 };
 
 #endif
